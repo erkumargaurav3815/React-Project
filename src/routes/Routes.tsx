25e4972b2a6bd8a4e73e357fa15add278a5526e4
erkumargaurav3815 <kumar.gaurav@habilelabs.io>
2026-07-15 14:40:27 +0530

    Added routes and protected routes as well.

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -4,13 +4,35 @@ import Courses from "../pages/Courses";
 import Contact from "../pages/Contact";
 import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
+import ProtectedRoute from "./ProtectedRoute";
 function RoutesComponent() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/courses" element={<Courses />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/courses"
+        element={
+          <ProtectedRoute>
+            <Courses />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/contact"
+        element={
+          <ProtectedRoute>
+            <Contact />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
     </Routes>
